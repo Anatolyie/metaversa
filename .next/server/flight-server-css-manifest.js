dec7b2ab44d08be699bfbb5853901e361f17ad59
Anatolii <anatolii.boboc@mail.ru>
2023-12-07 16:55:01 +0100

    metaversa proj

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/Users/anatoliiboboc/Desktop/metaversa/app/page":["/Users/anatoliiboboc/Desktop/metaversa/styles/globals.css"]},"/Users/anatoliiboboc/Desktop/metaversa/app/layout.js":["/Users/anatoliiboboc/Desktop/metaversa/styles/globals.css"]}
